55a5c1cbabf284750807a58a36c07678abe0f331
Minsun Kim <58019747+minisundev@users.noreply.github.com>
2023-10-24 12:44:11 +0900

    Add files via upload

---
+++ KioskApp/src/Menu.java
@@ -0,0 +1,38 @@
+public class Menu {
+    private String name;
+    private String desc;
+
+    public Menu() {
+    }
+
+    public Menu(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public void printDesc() {
+    }
+    public void printDescTotal(){}
+    public double getPrice() {
+        return 0;
+    }
+
+    public void setPrice(){}
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+
+    }
+
+    public String getDesc() {
+        return this.desc;
+    }
+    public void setDesc(String desc){
+        this.desc = desc;
+    }
+
+}
